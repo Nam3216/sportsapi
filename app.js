62ce87f6fc83039588f830953f5f80d5b27c4611
Nam3216 <96952383+Nam3216@users.noreply.github.com>
2022-10-22 17:17:37 -0300

    cors

--- app.js
+++ app.js
@@ -36,11 +36,20 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
+/*
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+  });*/
 
 
 //app.listen(PORT) //lo pongo para q funcione en railway
