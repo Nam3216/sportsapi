5687f7a512e245783d3d8670b95947f6d48b0cbf
Nam3216 <96952383+Nam3216@users.noreply.github.com>
2022-10-22 18:12:52 -0300

    new

--- routes/init.js
+++ routes/init.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const initController=require("../controllers/initController")
+const cors=require("cors");
+var express = require('express');
+var app = express();
+app.use(cors())
 
 
 /* GET users listing. */
