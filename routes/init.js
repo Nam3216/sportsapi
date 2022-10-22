17d66eff8b1d1e99c685b8fb8190219926bb1b38
Nam3216 <96952383+Nam3216@users.noreply.github.com>
2022-10-22 17:46:23 -0300

    cors

--- routes/init.js
+++ routes/init.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const initController=require("../controllers/initController")
+var cors = require('cors')
 
+app.use(cors()) // Use this after the variable declaration
 
 /* GET users listing. */
 router.get('/', initController.getAll);
