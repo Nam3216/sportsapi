6493dbdcc354bf74b7cec3aa5829ff1d7dc98c2b
Nam3216 <96952383+Nam3216@users.noreply.github.com>
2022-10-22 10:55:40 -0300

    db_port

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports={
  // lee primero desde sistema variable db_host (no lo da nueb, lo pongo yo cdo haga deploy, si no esvciste usa local host q es lo q uso cdo trabajajo localmente)
  DB_HOST:process.env.DB_HOST ||"localhost", //si no existe es porq trabajo local, entonces uso lo q esta en mysql.js
  DB_USER :process.env.DB_USER ||"root",//si no existe es porq trabajo local, entonces uso lo q esta en mysql.js
- DB_PASSWORD :process.env.DB_PASSWORD ||"stcollins01",//si no existe es porq trabajo local, entonces uso lo q esta en mysql.js
+ DB_PASSWORD:process.env.DB_PASSWORD ||"stcollins01",//si no existe es porq trabajo local, entonces uso lo q esta en mysql.js
  DB_NAME:process.env.DB_NAME||"sports",//si no existe es porq trabajo local, entonces uso lo q esta en mysql.js RAILQAY DA OTRO NOMBRE DE BASE DE DATOS
  DB_PORT:process.env.DB_PORT||3306,//si no existe es porq trabajo local, entonces uso lo q esta en mysql.js esto podria no estar, no lo tengo en mysql
 
