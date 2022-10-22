5f0ab88fca9ece616a00556b66d95607a7021a1b
Nam3216 <96952383+Nam3216@users.noreply.github.com>
2022-10-22 17:51:57 -0300

    new

--- app.js
+++ app.js
@@ -41,9 +41,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-var cors = require('cors')
 
-app.use(cors()) // Use this after the variable declaration
 
 /*
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var cors = require('cors')
 
-app.use(cors()) // Use this after the variable declaration
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
