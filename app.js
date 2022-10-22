e9b038b73e4b3673294c2a84324d4ccc623bdfe6
Nam3216 <96952383+Nam3216@users.noreply.github.com>
2022-10-22 17:22:33 -0300

    cors

--- app.js
+++ app.js
@@ -36,12 +36,16 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 /*
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
