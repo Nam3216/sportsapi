815c0bd4495cf24be9b00e9530754f7263408887
Nam3216 <96952383+Nam3216@users.noreply.github.com>
2022-10-22 10:42:19 -0300

    new

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //aca hay q poner la info que pongo en mysql.js, en el archivo en donde me conceto a la base de datos
 //hay q ponerlo en variables
 module.exports={
- PORT:process.env.DB_PORT || 3006,//aca digo q el puerto por defecto es 3000, el servicio web tipo railway puede dar otro. process es propio de reailw, se fija si existe PORT, SI ESTA es q el servicio ya dejo un entorno para conectarse, sino es 3000 del localhost
+ PORT:process.env.PORT || 3006,//aca digo q el puerto por defecto es 3000, el servicio web tipo railway puede dar otro. process es propio de reailw, se fija si existe PORT, SI ESTA es q el servicio ya dejo un entorno para conectarse, sino es 3000 del localhost
 
 //ahora comprobar para base de datos que me conecto desde mysql.js
 //el proveedor de la nube me da un usuario, un  pass, un dominio, un  puertto especial, una base de datos (porq un proveedor de nuebe a veces solo te permite crear base de datos en esa pag) asiq lo tengo q poner aca
